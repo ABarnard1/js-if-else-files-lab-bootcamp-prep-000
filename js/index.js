943c0a7b5609114b5afebc25f77615a50dc0735a
Alana Barnard <alanabarnard5@gmail.com>
2020-07-05 19:04:52 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ if(name === "Joe") {
   console.log("The name is correct")
 }
 var height = "74"
-else(height === "74") {
+if(height === "74") {
   console.log("The height is correct")
 }
 // Don't worry about this
